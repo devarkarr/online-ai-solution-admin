10c5d7b87e8dd5061b33d9ad64355b43ec4a196f
arkarlin2003 <arkarlin486@gmail.com>
2025-03-12 02:26:26 +0630

    feat(app): event card

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -12,12 +12,14 @@ import {
   Stack,
   Text,
 } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { IconCalendar, IconPlus, IconUser } from "@tabler/icons-react";
 import dayjs from "dayjs";
 
 const Event = () => {
+  const smallScreen = useMediaQuery("(max-width: 48em)");
+
   const { data: getEvents } = useGetEvents();
-  console.log(getEvents);
   return (
     <>
       <ContentLayout>
@@ -26,17 +28,27 @@ const Event = () => {
             New Event
           </Button>
         </Flex>
-        <Grid>
+        <Grid mt="lg">
           {getEvents?.map((event) => (
-            <Grid.Col span={4} key={event.id}>
+            <Grid.Col span={smallScreen ? 12 : 4} key={event.id}>
               <Card shadow="xs" padding="md" radius="md" withBorder>
                 <Card.Section>
                   <Image src={event.files[0].path} height={160} alt="Norway" />
                 </Card.Section>
 
                 <Group justify="space-between" mt="md" mb="xs">
-                  <Text fw={600}>Norway Fjord Adventures</Text>
-                  <Badge color="pink">{event.status}</Badge>
+                  <Text fw={600}>{event.name}</Text>
+                  <Badge
+                    color={
+                      event.status == "ONGOING"
+                        ? "var(--color-admin)"
+                        : event.status == "UPCOMING"
+                        ? "var(--accent-info)"
+                        : "var(--accent-danger)"
+                    }
+                  >
+                    {event.status}
+                  </Badge>
                 </Group>
                 <Stack gap={4}>
                   <Flex align="center" gap={4}>
@@ -50,13 +62,16 @@ const Event = () => {
                       {event.detail}
                     </Text>
                   </ScrollArea>
-                  <Flex>
+                  <Flex align="center" gap={4}>
                     <IconCalendar size={18} />
-                    <Flex>
-                      <Text>
+                    <Flex align="center" gap={4}>
+                      <Text fz="xs">
                         {dayjs(event.startDate).format("MMM D, YYYY")}
                       </Text>
-                      <Text>{dayjs(event.endDate).format("MMM D, YYYY")}</Text>
+                      {"-"}
+                      <Text fz="xs">
+                        {dayjs(event.endDate).format("MMM D, YYYY")}
+                      </Text>
                     </Flex>
                   </Flex>
                 </Stack>
